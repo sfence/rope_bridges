7c77c13ad13e8ef2a36eb7043b719ed25e529c70
SFENCE <sfence.software@gmail.com>
2021-10-26 22:01:41 +0200

    Fix bug in load configuration.

--- init.lua
+++ init.lua
@@ -7,8 +7,8 @@ local modpath = minetest.get_modpath(minetest.get_current_modname());
 
 -- settings
 rope_bridges.bridge_body_waving = minetest.settings:get_bool("rope_bridges_body_waving", true)
-rope_bridges.enabled_ropes = minetest.settings:get("palisade_enabled_ropes") or "*"
-rope_bridges.enabled_woods = minetest.settings:get("palisade_enabled_woods") or "*"
+rope_bridges.enabled_ropes = minetest.settings:get("rope_bridges_enabled_ropes") or "*"
+rope_bridges.enabled_woods = minetest.settings:get("rope_bridges_enabled_woods") or "*"
 
 dofile(modpath.."/ropes.lua")
 dofile(modpath.."/woods.lua")
